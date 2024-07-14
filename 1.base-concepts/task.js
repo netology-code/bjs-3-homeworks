e91f647fb8e7806ea6d630fa71641908358b3ce0
Vyacheslav <slavik.kurganov.26@mail.ru>
2024-07-14 19:29:57 +0300

    Переделал первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,11 +2,9 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
     arr =  [-b / ( 2 * a)];
-  } else {
+  } else if (d > 0) {
     arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
   }
   return arr;
