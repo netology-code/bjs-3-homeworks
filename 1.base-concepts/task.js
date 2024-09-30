97c61a97fabe8d9d81f6c936ea4fc5d005254cf3
Galya <galyakondr@mail.ru>
2024-09-30 16:35:40 +0300

    Initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  let p = percent / 100 / 12; //процентная ставка
+  let s = amount - contribution; //тело кредита
+  let n = countMonths;
+  let monthlyPaymen = s * (p + (p / (((1 + p) ** n) - 1))); //ежемесячная оплата
+  let totalAmount = Number((monthlyPaymen * n).toFixed(2));
+  return totalAmount
+
 }
